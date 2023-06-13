6c62f6b4e2e868a0a117e28554b4b732f33ce653
cooldudeguy420 <victor.the.pwner@gmail.com>
2023-06-13 10:46:28 +0200

    Satte DAC ind igen, og satte ADC på 48MHz!!!!!!!!!! med APCCHMAKS
    Sejt!

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@ constexpr int GENERATION_RATE = 51200;
 constexpr int ZERO = 511;
 
 // Constants for low pass filter
-constexpr int CUTOFF_FREQ = 80;
+constexpr int CUTOFF_FREQ = 50;
 
 constexpr float RC = 1.0 / (2 * PI * CUTOFF_FREQ);
 
@@ -43,9 +43,6 @@ volatile unsigned long previousTime = millis();
 
 volatile float frequency = 0;
 
-void ADCclockgoagane(){
-
-}
 
 // Set up clock for ADC. ADC clock is configured to run at 48MHz
 void ADCClockSetup() {
@@ -116,7 +113,7 @@ void TCC0Setup() {
 
   // Enable IRQ for TCC0
   NVIC_EnableIRQ(TCC0_IRQn);
-  NVIC_SetPriority(TCC0_IRQn,1);
+  NVIC_SetPriority(TCC0_IRQn,3);
 }
 
 // Set up ADC. ADC Listens on port A3 (PA04)
@@ -181,6 +178,106 @@ void ADCsetup() {
   ADC->CTRLA.bit.ENABLE = true;
 }
 
+void DACClockSetup() {
+    // Enable the 48MHz internal oscillator
+    SYSCTRL->DFLLCTRL.reg = SYSCTRL_DFLLCTRL_ENABLE;
+
+    // Setup clock GCLK5 for no div factor
+    GCLK->GENDIV.reg |= GCLK_GENDIV_ID(5) | GCLK_GENDIV_DIV(1);
+    while (GCLK->STATUS.reg & GCLK_STATUS_SYNCBUSY)
+        ;
+
+    // configure the generator of the generic clock, which is 48MHz clock
+    GCLK->GENCTRL.reg |= GCLK_GENCTRL_GENEN | GCLK_GENCTRL_SRC_DFLL48M |
+                         GCLK_GENCTRL_ID(5) | GCLK_GENCTRL_DIVSEL;
+    while (GCLK->STATUS.reg & GCLK_STATUS_SYNCBUSY)
+        ;
+
+    // enable clock, set gen clock number, and ID to where the clock goes (33 is
+    // DAC)
+    GCLK->CLKCTRL.reg |=
+            GCLK_CLKCTRL_CLKEN | GCLK_CLKCTRL_GEN(5) | GCLK_CLKCTRL_ID(33);
+    while (GCLK->STATUS.bit.SYNCBUSY)
+        ;
+}
+
+// Set up timer to run DAC.
+// This runs on GCLK6
+void TCC2Setup() {
+
+    // Run on general clock 6, divide by 1
+    GCLK->GENDIV.reg = GCLK_GENDIV_ID(6) | GCLK_GENDIV_DIV(1);
+
+    // Enable, set clock to DFLL 48 MHz clock source, select GCL6
+    GCLK->GENCTRL.reg =
+            GCLK_GENCTRL_GENEN | GCLK_GENCTRL_SRC_DFLL48M | GCLK_GENCTRL_ID(6);
+
+    // Wait for bus synchronization
+    while (GCLK->STATUS.bit.SYNCBUSY)
+        ;
+
+    // Enable, route GCLK6 to timer TCC2
+    GCLK->CLKCTRL.reg =
+            GCLK_CLKCTRL_CLKEN | GCLK_CLKCTRL_GEN_GCLK6 | GCLK_CLKCTRL_ID_TCC2_TC3;
+
+    // Set wave generation to match frequency
+    TCC2->WAVE.reg = TCC_WAVE_WAVEGEN_MFRQ;
+
+    // Wait for bus synchronization
+    while (TCC2->SYNCBUSY.bit.WAVE)
+        ;
+
+    // Set counter compare value based on clock speed and generation rate
+    TCC2->CC[0].reg = 48e6 / GENERATION_RATE;
+
+    // Wait for bus synchronization
+    while (TCC2->SYNCBUSY.bit.CC0)
+        ;
+
+    // Enable interrupt on compare match value
+    TCC2->INTENSET.bit.MC0 = 1;
+
+    // Enable TCC0
+    TCC2->CTRLA.bit.ENABLE = 1;
+
+    // Wait for bus synchronization
+    while (TCC2->SYNCBUSY.bit.ENABLE)
+        ;
+
+    // Enable IRQ for TCC0
+    NVIC_EnableIRQ(TCC2_IRQn);
+}
+
+inline void DACOn() {
+    // Enable DAC
+    DAC->CTRLA.bit.ENABLE = 1;
+}
+
+inline void DACOff() {
+    // Disable DAC
+    DAC->CTRLA.bit.ENABLE = 0;
+}
+
+// Set up DAC
+void DACSetup() {
+
+    // Set up timer before anything else
+    TCC2Setup();
+
+    DACClockSetup();
+
+    // Use analog voltage supply as reference selection
+    DAC->CTRLB.bit.REFSEL = 1;
+
+    // Enable output buffer
+    DAC->CTRLB.bit.EOEN = 1;
+
+    // Enable DAC
+    DACOn();
+}
+
+
+
 inline float lowPassFilter(float xn, float yn1) {
   return ALPHA * xn + (1 - ALPHA) * yn1;
 }
@@ -206,10 +303,10 @@ void ADC_Handler() {
 
   filterOutput =
       lowPassFilter((float)rawMeasurement, (float)previousFilterOutput);
-
   samplesPerPeriod++;
-  if ((previousFilterOutput < ZERO && filterOutput >= ZERO)/* ||
-      (previousFilterOutput > ZERO && filterOutput <= ZERO)*/) {
+
+
+  if ((previousFilterOutput < ZERO && filterOutput >= ZERO)) {
 
     interpolatedZeroCrossing = linearInterpolation(
         filterOutput, previousFilterOutput, samplesPerPeriod);
@@ -299,7 +396,7 @@ void loop() {
     Serial.print(", filterOutput=");
     Serial.print(filterOutput);
     Serial.print(", frequency=");
-    Serial.print(frequency);
+    Serial.print(frequency,4);
     Serial.print(", interpolatedZeroCrossing=");
     Serial.print(interpolatedZeroCrossing);
 
